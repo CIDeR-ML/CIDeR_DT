feeb1330459b227730924924b49d00141dc04784
Junjie Xia <junjie@sdfiana008.sdf.slac.stanford.edu>
2024-09-08 22:54:17 -0700

    update binning scheme

--- rebin_utils/binning.py
+++ rebin_utils/binning.py
@@ -1,10 +1,12 @@
 import numpy as np
 import h5py
 import sqlite3
-import pandas as pd
 import yaml
 import glob
 import os
+import math
+
+from datetime import datetime
 
 from wcprod.utils import voxels, directions
 from wcsim_utils.root_utils import rotate_wcte
@@ -19,12 +21,12 @@ class wc_binning():
             else:
                 cfg_gen = yaml.safe_load(cfg_gen)
             assert type(cfg_gen) is dict
-            self.r0_vox = float(cfg_gen['r0_vox'])
-            self.r1_vox = float(cfg_gen['r1_vox'])
-            self.z0_vox = float(cfg_gen['z0_vox'])
-            self.z1_vox = float(cfg_gen['z1_vox'])
-            self.phi0_vox = float(cfg_gen['phi0_vox'])
-            self.phi1_vox = float(cfg_gen['phi1_vox'])
+            self.r0_vox = float(cfg_gen['r0_Vox'])
+            self.r1_vox = float(cfg_gen['r1_Vox'])
+            self.z0_vox = float(cfg_gen['z0_Vox'])
+            self.z1_vox = float(cfg_gen['z1_Vox'])
+            self.phi0_vox = float(cfg_gen['phi0_Vox'])
+            self.phi1_vox = float(cfg_gen['phi1_Vox'])
 
     def configure(self, cfg):
         if type(cfg) == str:
@@ -61,22 +63,24 @@ class wc_binning():
         # Load the data from the h5 file
         with h5py.File(self.infile, 'r') as hf:
             for key in self.dset_names:
-                self.data[key] = hf['key'][:]
+                self.data[key] = hf[key][:]
 
     def create_grid(self):
         vox, pts = voxels(-self.zmax, self.zmax, 0, self.rmax, self.gap_space, self.n_phi_start)
-        dirs = directions(self.gap_angle, self.n_phi_start)
+        dirs = directions(self.gap_angle)
         #vox: 0:2 r-range, 2:4 phi-range, 4:6 z-range
         return vox, pts, dirs
 
     def create_db(self, pts, dirs):
+        if os.path.exists(self.dbfile):
+            return
         conn = sqlite3.connect(self.dbfile)
         c = conn.cursor()
-        c.execute('''CREATE TABLE bins IF NOT EXISTS bins
-                     (id INTEGER, x REAL, y REAL, z REAL, phi REAL, theta REAL, stats INTEGER, Qmax REAL)''',)
+        c.execute('''CREATE TABLE IF NOT EXISTS bins
+                     (id INTEGER, x REAL, y REAL, z REAL, phi REAL, theta REAL, stats INTEGER, Qmax REAL)''')
         for i, p in enumerate(pts):
             for j, d in enumerate(dirs):
-                c.execute("INSERT INTO bins VALUES (?,?,?,?,?,?,?,?)", (i*len(dirs)+j, p[0], p[1], p[2], d[0], d[1], 0, 0))
+                c.execute("INSERT INTO bins VALUES (?,?,?,?,?,?,?,?)", (i*len(dirs)+j, p[0], p[1], p[2], d[1], d[0], 0, 0))
         conn.commit()
         conn.close()
 
@@ -89,84 +93,102 @@ class wc_binning():
 
     def select_bins_from_db(self):
         conn = sqlite3.connect(self.dbfile)
+        conn.create_function("SQRT",  1, math.sqrt)
+        conn.create_function("POW",   2, math.pow)
+        conn.create_function("ABS",   1, math.fabs)
+        conn.create_function("ATAN2", 2, math.atan2)
         c = conn.cursor()
+
         query = '''
         SELECT id, x, y, z, phi, theta, stats, Qmax
         FROM bins
-        WHERE ((x**2+y**2)**0.5 > ? AND (x**2+y**2)**0.5 < ?
-        AND np.acos(x/(x**2+y**2)**0.5)*180./np.pi > ? AND np.acos(x/(x**2+y**2)**0.5)*180./np.pi < ?
-        AND z > ? AND z < ?)
-        OR ( 
-        (np.abs((x**2+y**2)**0.5 - ?) < 0.5*self.gap_space OR np.abs((x**2+y**2)**0.5 - ?) < 0.5*self.gap_space)
-        AND 
-        (np.abs(np.acos(x/(x**2+y**2)**0.5)*180./np.pi - ?) < 0.5*self.gap_angle OR np.abs(np.acos(x/(x**2+y**2)**0.5)*180./np.pi - ?) < 0.5*self.gap_angle)
-        AND
-        (np.abs(z - ?) < 0.5*self.gap_space OR np.abs(z - ?) < 0.5*self.gap_space)
-        );
+        WHERE ((SQRT(POW(x,2)+POW(y,2)) BETWEEN ? AND ?)
+        AND (x/SQRT(POW(x,2)+POW(y,2)) BETWEEN ? AND ?)
+        AND (y/SQRT(POW(x,2)+POW(y,2)) BETWEEN ? AND ?)
+        AND (z BETWEEN ? AND ?))
+        ;
         '''
-        c.execute(query, (self.r0_vox, self.r1_vox, self.phi0_vox, self.phi1_vox, self.z0_vox, self.z1_vox,
-                               self.r0_vox, self.r1_vox, self.phi0_vox, self.phi1_vox, self.z0_vox, self.z1_vox))
+        #OR (
+        #(ABS(SQRT(POW(x,2)+POW(y,2)) - ?) < ? OR ABS(SQRT(POW(x,2)+POW(y,2)) - ?) < ?)
+        #AND 
+        #((ABS((ATAN2(x, y)*180./? + 360.) % 360. - ?) < ?) OR (ABS((ATAN2(x, y)*180./? + 360.) % 360. - ?) < ?))
+        #AND
+        #(ABS(z - ?) < ? OR ABS(z - ?) < ?)
+        #self.r0_vox, 0.5*self.gap_space, self.r1_vox, 0.5*self.gap_space,
+        #math.pi, self.phi0_vox, 0.5*math.fabs(self.phi1_vox-self.phi0_vox), math.pi, self.phi1_vox, 0.5*math.fabs(self.phi1_vox-self.phi0_vox),
+        #self.z0_vox, 0.5*self.gap_space, self.z1_vox, 0.5*self.gap_space
+
+        c.execute(query, (self.r0_vox, self.r1_vox,
+                          min(math.cos(self.phi0_vox*math.pi/180.), math.cos(self.phi1_vox*math.pi/180.)), max(math.cos(self.phi0_vox*math.pi/180.), math.cos(self.phi1_vox*math.pi/180.)),
+                          min(math.sin(self.phi0_vox*math.pi/180.), math.sin(self.phi1_vox*math.pi/180.)), max(math.sin(self.phi0_vox*math.pi/180.), math.sin(self.phi1_vox*math.pi/180.)),
+                          self.z0_vox, self.z1_vox))
         self.rows = np.array(c.fetchall())
+        print(f"Fetched {len(self.rows)} bins that covers the generated voxel from db")
         conn.close()
 
     def process_dset(self):
 
-        alldata = np.column_stack(self.data['position'][:], self.data['direction'][:])
-        hit_pmt = self.data['digi_pmt'][:]
+        alldata = np.concatenate((self.data['position'][:]*10., self.data['direction'][:]), axis = 1)
+        hit_pmt = self.data['digi_pmt'][:]    
         hit_charge = self.data['digi_charge'][:]
-        hit_time = self.data['digi_time'][:])
+        hit_time = self.data['digi_time'][:]
 
+        mask = (hit_pmt >= 0) & (hit_pmt < self.npmt)
+        
         bindata = np.zeros(shape=(len(self.rows), 5), dtype=float)
-        sum_pmt = np.zeros(shape=(len(self.rows), self.npmt), dtype=float)
+        sum_hit = np.zeros(shape=(len(self.rows), self.npmt), dtype=float)
         sum_charge = np.zeros(shape=(len(self.rows), self.npmt), dtype=float)
         sum_time = np.zeros(shape=(len(self.rows), self.npmt), dtype=float)
 
-        bin_stats = np.zeros(shape=(len(self.rows), 1), dtype=int)
-        bin_Qmax = np.zeros(shape=(len(self.rows), 1), dtype=float)
-
-        min_dist = 999
-        min_angle = 999
-        min_idx = -1
+        bin_stats = np.zeros(shape=(len(self.rows),), dtype=int)
+        bin_Qmax = np.zeros(shape=(len(self.rows),), dtype=float)
 
         for i, d in enumerate(alldata):
             pos_rot = rotate_wcte(d[:3])
             dir_rot = rotate_wcte(d[3:])
-            dir_angle = np.array[np.arccos(dir_rot[0]/(dir_rot[0]**2+dir_rot[1]**2)**0.5, np.arccos(dir_rot[2])]
 
+            min_dist = self.gap_space
+            min_angle = 1.
+            min_idx = -1
+            
             for j, r in enumerate(self.rows):
-                if np.linalg.norm(r[1:4] - pos_rot) < min_dist and np.linalg.norm(r[4:6] - dir_angle) < min_angle:
-                    min_dist = np.linalg.norm(r[1:4] - pos_rot)
-                    min_angle = np.linalg.norm(r[4:6] - dir_angle)
+                bin_dir = np.array([np.cos(r[4]*np.pi/180.)*np.sin(r[5]*np.pi/180.), np.sin(r[4]*np.pi/180.)*np.sin(r[5]*np.pi/180.), np.cos(r[5]*np.pi/180.)])
+                dist_angle = np.linalg.norm(dir_rot - bin_dir)
+                if (np.linalg.norm(r[1:4] - pos_rot) <= min_dist) and (dist_angle <= min_angle):
+                    min_dist = np.linalg.norm(r[1:4] - pos_rot)                    
+                    min_angle = dist_angle
                     min_idx = j
 
             if min_idx == -1:
                 continue
             else:
-                bindata[min_idx, 0:5] = self.rows[min_idx][1:6]
-                sum_hit[min_idx][hit_pmt[i]] += 1
-                sum_charge[min_idx][hit_pmt[i]] += hit_charge[i]
-                sum_time[min_idx][hit_pmt[i]] += hit_time[i]
-
+                bindata[min_idx] = self.rows[min_idx][1:6]
+                sum_hit[min_idx][hit_pmt[i][mask[i]]] += 1
+                sum_charge[min_idx][hit_pmt[i][mask[i]]] += hit_charge[i][mask[i]]
+                sum_time[min_idx][hit_pmt[i][mask[i]]] += hit_time[i][mask[i]]
                 bin_stats[min_idx] += 1
-                bin_Qmax[min_idx] = max(bin_Qmax[min_idx], max(hit_charge[i]))
+                bin_Qmax[min_idx] = max(bin_Qmax[min_idx], np.max(sum_charge[min_idx])/bin_stats[min_idx])
 
 
         conn = sqlite3.connect(self.dbfile)
         c = conn.cursor()
         sql_update_query = """UPDATE bins
                               SET stats = ?, Qmax = ?
-                              WHERE idx = ?"""
-        for r in enumerate(self.rows):
-            c.execute(sql_update_query, (bin_stats[i], bin_Qmax[i], r[0]))
+                              WHERE id = ?"""
+        for i, r in enumerate(self.rows):
+            print(i, bin_stats[i], bin_Qmax[i])
+            c.execute(sql_update_query, (bin_stats[i]+r[-2], max(bin_Qmax[i],r[-1]), r[0]))
         conn.commit()
         conn.close()
 
-        sum_time /= sum_hit
+        sum_hit[bin_stats>0] /= bin_stats[bin_stats>0][:, np.newaxis]
+        sum_charge[bin_stats>0] /= bin_stats[bin_stats>0][:, np.newaxis]
+        sum_time[bin_stats>0] /= bin_stats[bin_stats>0][:, np.newaxis]
 
 
         self.dset['vertices']   = self.fh5.create_dataset('vertices', data=bindata.copy(),
                                                           compression=self.cmprs, compression_opts=self.cmprs_opt)
-        self.dset['hit_pmt']    = self.fh5.create_dataset('hit_pmt', data=sum_pmt.copy(),
+        self.dset['hit_pmt']    = self.fh5.create_dataset('hit_pmt', data=sum_hit.copy(),
                                                           compression=self.cmprs, compression_opts=self.cmprs_opt)
         self.dset['hit_charge'] = self.fh5.create_dataset('hit_charge', data=sum_charge.copy(),
                                                           compression=self.cmprs, compression_opts=self.cmprs_opt)
@@ -174,3 +196,4 @@ class wc_binning():
                                                           compression=self.cmprs, compression_opts=self.cmprs_opt)
         self.fh5.close()
         print(f"Written to h5 file {self.outfile}.")
+
